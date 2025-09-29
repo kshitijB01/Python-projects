7308e8b43b06bfba28d2ad5d6aa48fc760474164
KSHITIJ BOKDE <kshitijliladharbokde@gmail.com>
2025-09-29 15:47:01 +0530

    Delete Username generator directory

--- Username generator/Username_Generator.py
+++
@@ -1,6 +0,0 @@
-#Username Generator using Firstname and Birth year
-Firstname = input("Hey Buddy, Enter your First-Name: ")
-Birthyear = int(input("Enter your Birth year too: "))
-
-Username = f"{Firstname}{Birthyear}"
-print(f"Your Username is: {Username}") 
