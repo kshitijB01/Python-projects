47b08cc0a8940b67190ef80978456e2783f1cf57
Kshitij Bokde <kshitijliladharbokde@gmail.com>
2025-09-29 15:17:00 +0530

    Add Username generator project

---
+++ Username generator/Username_Generator.py
@@ -0,0 +1,6 @@
+#Username Generator using Firstname and Birth year
+Firstname = input("Hey Buddy, Enter your First-Name: ")
+Birthyear = int(input("Enter your Birth year too: "))
+
+Username = f"{Firstname}{Birthyear}"
+print(f"Your Username is: {Username}") 
